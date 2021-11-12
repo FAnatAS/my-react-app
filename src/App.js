ea87b821dea1df365d15e9d4f74a4ccc44a04687
FAnatAS <anatoliy.s.fedorenko@gmail.com>
2021-11-12 10:41:41 +0200

    one of commits 121121

--- src/App.js
+++ src/App.js
@@ -1,39 +1,22 @@
 // import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {Component} from 'react';
 import Ciao from './components/Ciao';
 
-function App() {
-  // return (
-  //   <div className="App">
-  //     <header className="App-header">
-  //       <img src={logo} className="App-logo" alt="logo" />
-  //       <p>
-  //         Edit <code>src/App.js</code> and save to reload.
-  //       </p>
-  //       <a
-  //         className="App-link"
-  //         href="https://reactjs.org"
-  //         target="_blank"
-  //         rel="noopener noreferrer"
-  //       >
-  //         Learn React
-  //       </a>
-  //     </header>
-  //   </div>
-  // );
-//return <h1>Hi, JSX! </h1>
-
-const user = {
-  fname:'Elon',
-  sname: 'Musk'
-}
-return <>
-<Ciao name={`${user.fname} ${user.sname}`} />
-<Ciao name='Elen' />
-<Ciao name='Tim Lee' />
-</>
+class App extends Component {
+  constructor (props){
+    super(props);
+    this.state = {
+      fname:'Elon',
+      sname: 'Musk'
+    }
+  }
+render(){
+  const {fname}=this.state;
+  return <>
+  <Ciao name={fname}/>  
+  </>
 
 }
-
+}
 export default App;
